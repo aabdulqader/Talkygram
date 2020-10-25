7b572c13f64359f1e0a84cfa3ff28604c5dbe715
shimul-aq <shimul.official7@gmail.com>
2020-10-26 02:58:57 +0600

    first commit

---
+++ talkygram/po.md
@@ -0,0 +1,12 @@
+1. Profiles
+    - Profile
+    - relationship
+
+
+2. Post
+    - Post
+    - Comment
+    - Like
+
+
+3. allauth (authentications)
---
+++ talkygram/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name = 'home'),
+    path('profiles/', include('profiles.urls', namespace = 'profiles')),
+
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
