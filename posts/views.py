43843289e94c00d16b1de189e357ee633977ff2e
shimul-aq <shimul.official7@gmail.com>
2020-10-27 13:15:19 +0600

    Update & Delete Posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from .models import Post, Profile, Like
 from .forms import PostModelForm, CommentModelForm
+from django.views.generic import UpdateView, DeleteView
+from django.contrib import messages
+
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "posts/confirm_del.html"
+    success_url = reverse_lazy('posts:main_post_view')
+    #success_url = '/posts/'
+
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if not obj.author.user == self.request.user:
+            messages.warning(self.request, 'You\'r not the right author of this post. You ain\'t able to DELETE this post')
+        return obj
+    
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostModelForm
+    template_name = 'posts/update.html'
+    success_url = reverse_lazy('posts:main_post_view')
+
+    def form_valid(self, form):
+        profile = Profile.objects.get(user=self.request.user)
+        if form.instance.author == profile:
+            return super().form_valid(form)
+        else:
+            form.add_error(None, "You\'r not the right author of this post. You ain\'t able to UPDATE this post")
+            return super().form_valid(form)
 
 
 
