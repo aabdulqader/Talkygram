00a972519ad47b9d9065c211e91248dda54dc433
shimul-aq <shimul.official7@gmail.com>
2020-10-27 20:29:52 +0600

    pre_delete signal, send invitations, remove frends

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,8 @@ from .views import (
     profiles_list_view, 
     invite_profiles_list_view,
     ProfileListView,
+    send_invitation,
+    remove_from_friends,
 
 
 )
@@ -17,8 +19,8 @@ urlpatterns = [
     path('my-invites/', invites_received_view, name = 'my_invites'),
     path('all-profiles/', ProfileListView.as_view(), name = 'all_profiles'),
     path('to-invite/', invite_profiles_list_view, name = 'invite_profiles'),
-
-
+    path('send-invite/', send_invitation, name = 'send_invite'),
+    path('remove-friend/', remove_from_friends, name = 'remove_friend'),
 
 ]
 
